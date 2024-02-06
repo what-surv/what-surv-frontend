a465b5e5ea061e6ff9b6a75e2c55fb7416f52022
bomkyu <a01090762806@gmail.com>
2024-02-06 18:17:53 +0900

    style : 1.index.css 자간 -1로 변경

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import style from './login.module.css';
+import { KakaoLogin } from '../stories/social/kakaologin/KakaoLogin';
+import { GoogleLogin } from '../stories/social/googlelogin/GoogleLogin';
+import { NaverLogin } from '../stories/social/naverlogin/NaverLogin';
 
 const Login = () => {
   return (
@@ -11,15 +14,9 @@ const Login = () => {
         </div>
         <div className='flex flex-col max-w-xl w-full'>
           <p className='text-lg font-bold'>서비스명에 오신 것을 환영합니다!</p>
-          <button type='button' className={`${style['login-btn']}`}>
-            카카오로 시작하기
-          </button>
-          <button type='button' className={`${style['login-btn']}`}>
-            구글로 시작하기
-          </button>
-          <button type='button' className={`${style['login-btn']}`}>
-            네이버로 시작하기
-          </button>
+          <KakaoLogin size='full' children='카카오로 시작하기' />
+          <GoogleLogin size='full' children='구글로 시작하기' />
+          <NaverLogin size='full' children='네이버로 시작하기' />
         </div>
       </div>
     </div>
