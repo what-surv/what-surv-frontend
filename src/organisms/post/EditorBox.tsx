693fe8fc56bc7551d59d8b7f43f0b716f3eba094
Jaesin22 <jaesin22@naver.com>
2024-02-17 16:22:08 +0900

    chore. toast ui editor 제거, CKEditor install

--- src/organisms/post/EditorBox.tsx
+++ src/organisms/post/EditorBox.tsx
@@ -1,19 +1,5 @@
-import '@toast-ui/editor/dist/toastui-editor.css';
-
-import { Editor } from '@toast-ui/react-editor';
-
 const EditorBox = () => {
-  return (
-    <div className='prose' style={{ backgroundImage: 'inherit' }}>
-      <Editor
-        initialValue='test'
-        previewStyle='tab'
-        height='600px'
-        initialEditType='wysiwyg'
-        hideModeSwitch
-      />
-    </div>
-  );
+  return <div className='prose' style={{ backgroundImage: 'inherit' }} />;
 };
 
 export default EditorBox;
