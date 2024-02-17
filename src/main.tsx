422592fe30787b5e90ab2cf35c8122410949390b
Jaesin22 <jaesin22@naver.com>
2024-02-17 17:46:15 +0900

    chore. toast ui editor로 다시 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import './index.css';
 
 import App from './App.tsx';
 
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- src/organisms/post/EditorBox.tsx
+++ src/organisms/post/EditorBox.tsx
@@ -1,5 +1,16 @@
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
 const EditorBox = () => {
-  return <div className='prose' style={{ backgroundImage: 'inherit' }} />;
+  return (
+    <Editor
+      initialValue='hello react editor world!'
+      previewStyle='vertical'
+      height='600px'
+      initialEditType='wysiwyg'
+      useCommandShortcut={false}
+    />
+  );
 };
 
 export default EditorBox;
