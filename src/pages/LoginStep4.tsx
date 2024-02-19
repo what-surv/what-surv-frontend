497044ec4d3bc9c21c8a1700fd12b2db0ec83acb
bomkyu <a01090762806@gmail.com>
2024-02-19 17:12:10 +0900

    fix : 기존코드 복원

--- src/pages/LoginStep4.tsx
+++ src/pages/LoginStep4.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 import style from './login.module.css';
 import icSuccessBig from '../stories/assets/ic-success-big.svg';
 
+import { useNavigate } from 'react-router-dom';
+
 const LoginStep4 = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <p className='text-lg text-center text-center font-bold mb-[24px] mt-5'>
@@ -13,7 +16,13 @@ const LoginStep4 = () => {
         <img className='inline' src={icSuccessBig} alt='회원가입 성공 아이콘' />
       </div>
       <p className='text-center my-4'>회원가입이 완료되었습니다.</p>
-      <button type='button' className={style['basic-btn']}>
+      <button
+        type='button'
+        className={style['basic-btn']}
+        onClick={() => {
+          navigate('/main');
+        }}
+      >
         서비스명 시작하기
       </button>
     </div>
