357b0fa7e83f953a328b1865ab2f83d9ddc5d935
bomkyu <a01090762806@gmail.com>
2024-03-18 23:35:44 +0900

    fix. style삭제

--- src/stories/card/Card.stories.ts
+++ src/stories/card/Card.stories.ts
@@ -14,13 +14,6 @@ const meta = {
 
 export default meta;
 
-// 여기에서 컨테이너 스타일을 정의합니다.
-const CardContainerStyle = {
-  width: '500px', // 원하는 너비로 조정하세요.
-  padding: '20px', // 필요에 따라 패딩 등 추가 스타일을 적용할 수 있습니다.
-  boxSizing: 'border-box', // 패딩이 너비에 포함되도록 설정
-};
-
 export const Main: StoryObj<typeof Card> = {
   args: {
     children: '제목입니다. 가나다라마바사아자차카타파하. 몇자까지 넣을까',
