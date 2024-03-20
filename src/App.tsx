36ca504c186148f3e343bd9f241f2899d2802926
bomkyu <a01090762806@gmail.com>
2024-03-20 22:41:27 +0900

    fix. PrivateRoute 누락된 페이지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Index />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/write' element={<PostWritePage />} />
-          <Route path='/edit/:postId' element={<PostEditPage />} />
+
           <Route path='/login/new-user' element={<Login />} />
           <Route path='/login/success' element={<Login />} />
           <Route path='/login/failure' element={<Login />} />
@@ -42,6 +41,8 @@ const App = () => {
           <Route path='/termsOfService' element={<TermsofServicePage />} />
           <Route path='/lite' element={<Lite />} />
           <Route element={<PrivateRoute />}>
+            <Route path='/write' element={<PostWritePage />} />
+            <Route path='/edit/:postId' element={<PostEditPage />} />
             <Route path='/me/setting' element={<Setting />} />
             <Route path='/me/likes' element={<InterestArticlesPage />} />
             <Route path='/me/posts' element={<MyWritePostPage />} />
