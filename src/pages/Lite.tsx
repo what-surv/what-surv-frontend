844529c6cab6cc273ca96bcf85b053eef50fb7cd
bomkyu <a01090762806@gmail.com>
2024-03-22 14:31:47 +0900

    fix. IT페이지 footer 위치 설정

--- src/pages/Lite.tsx
+++ src/pages/Lite.tsx
@@ -1,5 +1,6 @@
 import CommingSoon from './misc/CommingSoon';
 import { Appbar } from '../stories/appbar/Appbar';
+import Footer from '../stories/footer/Footer';
 import { Tabbar } from '../stories/tabbar/Tabbar';
 
 const Lite = () => {
@@ -7,9 +8,10 @@ const Lite = () => {
     <div>
       <Appbar isAccount isLogo isFullLogo />
       <Tabbar isMobileVisible size='default' />
-      <div className='max-w-[770px] w-full m-auto'>
+      <div className='max-w-[770px] w-full h-[calc(100vh_-_462px)] m-auto'>
         <CommingSoon />
       </div>
+      <Footer />
     </div>
   );
 };
