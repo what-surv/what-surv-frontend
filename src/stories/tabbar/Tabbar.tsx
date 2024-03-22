6f78a004d1459e7025b30df3f214a6069176c3d1
bomkyu <a01090762806@gmail.com>
2024-03-22 14:16:59 +0900

    fix. tabbar Active되게 수정

--- src/stories/tabbar/Tabbar.tsx
+++ src/stories/tabbar/Tabbar.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 import Typography from '../typography/Typography';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const TabbarVariants = cva(
   `w-full min-w-[280px] bg-[#FAFAFA] h-[56px] flex items-end`,
@@ -33,32 +33,42 @@ interface TabbarProps {
  */
 
 export const Tabbar = ({ size, isMobileVisible, ...props }: TabbarProps) => {
+  const location = useLocation();
+  // 현재 URL 경로와 링크의 경로가 일치하는지 확인하는 함수
+  const isActiveLink = (path: string) => location.pathname === path;
+
   return (
     <header
       className={`${TabbarVariants({ size, ...props })} justify-between w-full px-6 ${isMobileVisible ? `` : `hidden md:block`} md:px-[180px]`}
     >
       <div className='flex max-w-[1560px] w-full m-auto items-center logo gap-7'>
         <Link
-          className='py-2 px-4 w-12 md:w-16 flex-col items-stretch flex gap-2.5 border-b-2 border-b-[#0051FF]'
+          className='relative py-2 px-4 w-12 md:w-16 flex-col items-stretch flex gap-2.5'
           to='/'
         >
           <Typography
             text='IT'
             size='base'
             weight='Semibold'
-            className='text-base font-semibold leading-[26px] text-[#0051FF]'
+            className='text-base font-semibold leading-[26px] text-[#0051FF] text-center'
+          />
+          <div
+            className={`absolute bottom-0 left-0 w-0 h-[2px] bg-[#0051FF] ${isActiveLink('/') ? 'w-[100%]' : ''}  transition-all duration-300 ease-in`}
           />
         </Link>
         <div>
           <Link
             to='/lite'
-            className='py-2 w-12 md:w-16 px-4 flex-col items-stretch flex gap-2.5'
+            className='relative py-2 w-12 md:w-16 px-4 flex-col items-stretch flex gap-2.5'
           >
             <Typography
               text='Lite'
               size='base'
               weight='Semibold'
-              className='text-base font-semibold leading-[26px] text-[#545760]'
+              className='text-base font-semibold leading-[26px] text-[#545760] text-center'
+            />
+            <div
+              className={`absolute bottom-0 left-0 w-0 h-[2px] bg-[#0051FF] ${isActiveLink('/lite') ? 'w-[100%]' : ''}  transition-all duration-300 ease-in`}
             />
           </Link>
         </div>
