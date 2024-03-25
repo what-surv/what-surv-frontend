b4b25947fd0275d27d1db58528c49a5a5d82e5eb
bomkyu <a01090762806@gmail.com>
2024-03-25 13:46:45 +0900

    fix. alert 버튼 비율 수정

--- src/organisms/LoginAlertModal.tsx
+++ src/organisms/LoginAlertModal.tsx
@@ -21,35 +21,43 @@ const LoginAlertModal = ({
       <div className='fixed top-0 left-0 z-50 flex items-center flex-shrink-0 justify-center w-full h-screen mx-auto bg-[#242424] opacity-70' />
       <div className='fixed top-0 left-0 z-50 flex items-center justify-center flex-shrink-0 w-full h-screen mx-auto'>
         <div className='flex text-center w-[90%] justify-center items-center max-w-[504px] bg-[#E5EEFF] gap-6 iphone:p-10 fold:p-6 flex-col rounded-2xl'>
-          <Typography
-            size='xl'
-            text='로그인 후 이용할 수 있는 기능이에요.'
-            weight='Bold'
-          />
-          <Typography
-            size='base'
-            text='로그인 하시겠어요?'
-            weight='Medium'
-            lineheight={26}
-            className='whitespace-pre-wrap '
-          />
-          <div className='flex items-center self-stretch justify-center gap-2'>
-            <button
-              type='button'
-              onClick={goLogin}
-              className='rounded-[400px] flex border-2 whitespace-nowrap  border-[#0051FF] text-white items-center py-3 px-6 bg-[#0051FF] flex-col gap-2.5 flex-1 justify-center'
-            >
-              간편 로그인하기
-            </button>
-            {handleClose !== undefined && (
+          <div className='max-w-[342px] w-full m-auto'>
+            <div className='flex flex-col gap-6'>
+              <p>
+                <Typography
+                  size='xl'
+                  text='로그인 후 이용할 수 있는 기능이에요.'
+                  weight='Bold'
+                />
+              </p>
+              <p>
+                <Typography
+                  size='base'
+                  text='로그인 하시겠어요?'
+                  weight='Medium'
+                  lineheight={26}
+                  className='whitespace-pre-wrap '
+                />
+              </p>
+            </div>
+            <div className='LoginButton self-stretch justify-center items-center gap-2 flex mt-4'>
+              {handleClose !== undefined && (
+                <button
+                  type='button'
+                  onClick={handleClose}
+                  className='flex w-[86px] sm:w-[114px] h-[50px] bg-neutral-50 rounded-[400px] border-2 border-blue-600 flex-col justify-center items-center gap-2.5 '
+                >
+                  아니요
+                </button>
+              )}
               <button
                 type='button'
-                onClick={handleClose}
-                className='rounded-[400px] bg-[#FAFAFA] text-[#242424] whitespace-nowrap flex items-center py-3 px-6 border-2 flex-col gap-2.5 flex-1 justify-center border-[#0051FF]'
+                onClick={goLogin}
+                className='flex grow w-[calc(100%-86px)] sm:w-[calc(100%-114px)] h-[50px] bg-blue-600 rounded-[400px] flex-col justify-center items-center gap-2.5 text-[#FFFFFF]'
               >
-                취소
+                간편 로그인하기
               </button>
-            )}
+            </div>
           </div>
         </div>
       </div>
