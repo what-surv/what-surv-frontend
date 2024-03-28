3e0fba4abf73755a4a49fce59523a821bf6c53d5
Jaesin22 <jaesin22@naver.com>
2024-03-28 16:01:31 +0900

    react-ga4 추가(제발..)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
 import './index.css';
+
+import ReactGA from 'react-ga4';
 
 import App from './App.tsx';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
+ReactGA.initialize('G-LRGMTHCDDL');
 
 const queryClient = new QueryClient({
   defaultOptions: {
