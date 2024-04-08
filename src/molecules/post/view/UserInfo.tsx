24c8ce517476fd0e4f1495a2cc513190787b98bf
Jaesin22 <jaesin22@naver.com>
2024-04-08 17:58:27 +0900

    fix. 게시글 상세 조회 시 삭제된 유저의 댓글일 때 에러나는 문제 수정

--- src/molecules/post/view/UserInfo.tsx
+++ src/molecules/post/view/UserInfo.tsx
@@ -1,11 +1,18 @@
 import outlineAccount from '../../../assets/account-outline.svg';
 import Typography from '../../../stories/typography/Typography';
 
-const UserInfo = ({ nickname }: { nickname: string }) => {
+const UserInfo = ({ nickname }: { nickname: string | null }) => {
+  const displayName = nickname || '탈퇴한 유저';
+
   return (
     <div className='flex items-center self-stretch gap-1'>
       <img src={outlineAccount} alt='계정 로고' className='p-2.5' />
-      <Typography text={nickname} size='sm' weight='Semibold' lineheight={22} />
+      <Typography
+        text={displayName}
+        size='sm'
+        weight='Semibold'
+        lineheight={22}
+      />
     </div>
   );
 };
