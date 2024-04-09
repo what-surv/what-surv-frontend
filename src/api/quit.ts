e97a51c485218327449de73161381ae992448706
Jaesin22 <jaesin22@naver.com>
2024-04-09 15:41:43 +0900

    사라진 조건문 추가

--- src/api/quit.ts
+++
@@ -1,5 +0,0 @@
-import { axiosBaseUrl } from './axiosConfig';
-
-export const postQuit = async (data: string) => {
-  await axiosBaseUrl.post(`/auth/quit`, data);
-};
--- src/stories/appbar/util.ts
+++
@@ -1,5 +0,0 @@
-export const menuItems = [
-  { id: 1, label: '관심 표시한 글', url: '/me/likes' },
-  { id: 2, label: '내 모집글', url: '/me/posts' },
-  { id: 3, label: '설정', url: '/me/setting' },
-];
