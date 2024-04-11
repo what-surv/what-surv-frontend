5635f67467a882b3edb348d46171c82e72789699
Jaesin22 <jaesin22@naver.com>
2024-04-12 00:39:02 +0900

    fix. 내가 관심가진 글/내가 작성한 글 로그아웃 모달 붙이기

--- src/pages/myPages/InterestArticlesPage.tsx
+++ src/pages/myPages/InterestArticlesPage.tsx
@@ -1,7 +1,14 @@
+import { useEffect, useState } from 'react';
+
+import { requestLogout } from '../../api/loginApis';
+import { userCheckApi } from '../../api/userCheckApi';
 import PostListSelect from '../../molecules/PostListSelect';
+import LogoutAlertModal from '../../organisms/LogoutAlertModal';
 import LikePostList from '../../organisms/post/mypage/LikePostList';
 import { Appbar } from '../../stories/appbar/Appbar';
 import { Tabbar } from '../../stories/tabbar/Tabbar';
+
+import { useLocation } from 'react-router-dom';
 
 interface ButtonInfo {
   label: string;
@@ -14,10 +21,41 @@ const buttonValues: ButtonInfo[] = [
 ];
 
 const InterestArticlesPage = () => {
+  const location = useLocation();
+  // LogoutAlertModal을 제어하기 위한 상태
+  const [showLogoutAlert, setShowLogoutAlert] = useState(false);
+  // 사용자 로그인 상태를 저장하기 위한 상태 변수
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (location.state?.quit) {
+      setIsLoggedIn(false);
+    } else {
+      const fetchUserStatus = async () => {
+        const userStatus = await userCheckApi();
+        setIsLoggedIn(userStatus);
+      };
+
+      fetchUserStatus();
+    }
+  }, []);
   return (
     <div className='w-full'>
       <div className='w-full header'>
-        <Appbar isLogo isAccount />
+        {isLoggedIn ? (
+          <div>
+            <Appbar
+              isAccount
+              isLogo
+              isFullLogo
+              logout={() => {
+                setShowLogoutAlert(true);
+              }}
+            />
+          </div>
+        ) : (
+          <Appbar isLogo isFullLogo isLogin />
+        )}
         <Tabbar />
       </div>
       <div className='pt-4 md:pt-14 mx-auto w-[90%] md:w-[85%]'>
@@ -29,6 +67,15 @@ const InterestArticlesPage = () => {
           <LikePostList />
         </div>
       </div>
+      <LogoutAlertModal
+        isOpen={showLogoutAlert}
+        handleClose={() => setShowLogoutAlert(false)}
+        goLogout={async () => {
+          await requestLogout();
+          setIsLoggedIn(false);
+          setShowLogoutAlert(false);
+        }}
+      />
     </div>
   );
 };
